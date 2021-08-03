dd7bd30d91d6e89f8e1bb1a2b2a9cc496dc3f9f3
Natalie <ngmr@bk.ru>
2021-08-03 20:23:32 +0300

    add mailer, animation

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   $('.carousel__inner').slick({
     speed: 1200,
-    prevArrow: '<button type="button" class="slick-prev"><img src="/icons/left.svg"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="/icons/right.svg"></button>',
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
     responsive: [
       {
         breakpoint: 992,
@@ -79,5 +79,37 @@ $(document).ready(function(){
   validateForms('#order form');
 
   $('input[name=phone]').mask("+7 (999) 999-99-99");
-});
-   
+
+  $('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+
+        $('form').trigger('reset');
+    });
+    return false;
+  });
+
+  //skroll
+  $(window).scroll(function() {
+    if ($(this).scrollTop() >1600) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+  $("a[href^='#']").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+  });
+  
+  new WOW().init();
+
+}); 
